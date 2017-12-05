3231ad5da0c461b9bd63971b599bd72bf3f8f433
Eric Robert <3ric.robert@gmail.com>
2017-12-05 14:46:24 -0500

    day 5b

--- 5/main.go
+++ 5/main.go
@@ -34,7 +34,12 @@ func Main(args []string) {
 		ints = append(ints, i)
 	}
 
-	fmt.Print(Run(ints))
+	switch args[0] {
+	case "5a", "5":
+		fmt.Print(Run(ints))
+	case "5b":
+		fmt.Print(Run2(ints))
+	}
 }
 
 func Run(jumps []int) int {
@@ -43,6 +48,29 @@ func Run(jumps []int) int {
 	for {
 		k := jumps[i]
 		jumps[i]++
+		n++
+		i += k
+
+		if i < 0 || i >= len(jumps) {
+			break
+		}
+	}
+
+	return n
+}
+
+func Run2(jumps []int) int {
+	n, i := 0, 0
+
+	for {
+		k := jumps[i]
+
+		if k < 3 {
+			jumps[i]++
+		} else {
+			jumps[i]--
+		}
+
 		n++
 		i += k
 
--- 5/main_test.go
+++ 5/main_test.go
@@ -22,3 +22,22 @@ func TestRun(t *testing.T) {
 		}
 	}
 }
+
+func TestRun2(t *testing.T) {
+	tests := []struct {
+		Table  []int
+		Result int
+	}{
+		{
+			Table:  []int{0, 3, 0, 1, -3},
+			Result: 10,
+		},
+	}
+
+	for i, test := range tests {
+		n := Run2(test.Table)
+		if n != test.Result {
+			t.Fatalf("%d: %d (should be %d)", i, n, test.Result)
+		}
+	}
+}
